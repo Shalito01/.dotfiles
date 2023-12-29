498c38492843cb716282fbaf7f9424f1954fec0b
Daniel Shala <daniel.shala08@gmail.com>
2023-12-29 16:47:05 +0100

    Update lsp.lua

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -3,9 +3,7 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'omnisharp',
-  'lua-language-server',
-  'cmake-language-server'
+  'omnisharp'
 })
 
 -- Fix Undefined global 'vim'
